201d411d6ef5c8da215d82b7bd5d395a3490710c
farahbed <farah.bendriss.la@gmail.com>
2024-11-16 18:21:28 +0100

    create page about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,37 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi everyone, I’m <span className="purple">Farah Bendriss</span>, from 
+            <span className="purple"> Épinay-sous-Sénart, France</span>.
             <br />
-            I am currently employed as a software developer at Juspay.
+            Passionate about web development, I am currently looking for a front-end developer position. I recently 
+            completed a comprehensive front-end development training at OpenClassroom, where I gained strong skills in 
+            HTML, CSS, JavaScript, and React.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            <br />
+            What excites me about development is the ability to create seamless and interactive user experiences, 
+            leveraging the latest technologies. I particularly enjoy working on projects where I can solve real-world problems 
+            while constantly learning and improving.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Outside of coding, I love exploring new horizons. I’m especially passionate about:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Exploring innovative technologies and web development trends
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Reading books, especially on technology and personal development
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Traveling to discover new cultures and draw inspiration from different lifestyles
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "My goal is to create innovative digital solutions that bring real value and enhance the user experience."
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Farah</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,11 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
@@ -16,40 +12,25 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              LET ME <span className="purple">INTRODUCE</span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              Passionate about web development, I enjoy creating innovative products
+              and using the latest technologies to make the web more interactive and
+              accessible. 🚀
               <br />
-              <br />I am fluent in classics like
+              <br />I specialize in technologies like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> JavaScript, React.js, and Next.js. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
+              My focus is particularly on building web applications and modern solutions
+              with tools like <b className="purple">Node.js</b>.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              I love tackling technical challenges and working on innovative projects
+              that push the boundaries of web development.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -62,47 +43,27 @@ function Home2() {
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className="purple">contact me</span>!
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/farahbed"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.linkedin.com/in/farah-bendriss-614807171/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
                 >
-                  <AiFillInstagram />
+                  <FaLinkedinIn />
                 </a>
               </li>
             </ul>
@@ -112,4 +73,6 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
+
