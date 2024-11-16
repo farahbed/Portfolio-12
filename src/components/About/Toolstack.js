57b40991e8abcaf46606982c0cc2207c08eddc5a
farahbed <farah.bendriss.la@gmail.com>
2024-11-16 18:41:37 +0100

    page about with the skill completed

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
   SiSlack,
-  SiVercel,
   SiMacos,
+  SiSwagger,
+  SiGithub,
+  SiDiscord,
 } from "react-icons/si";
 
 function Toolstack() {
@@ -18,16 +19,22 @@ function Toolstack() {
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiSwagger />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithub />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDiscord />
       </Col>
     </Row>
   );
 }
 
 export default Toolstack;
+
+
+
