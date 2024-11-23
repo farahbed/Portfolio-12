a9e7142d061b2729a2485d4ad460c1643d1b807e
farahbed <farah.bendriss.la@gmail.com>
2024-11-23 19:13:23 +0100

    portfolio projet fini

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,9 +4,9 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
 // Import des images de projets
-import kasa from "../../Assets/Projects/kasa.png";
+import ninacarducci from "../../Assets/Projects/ninacarducci.png";
 import booki from "../../Assets/Projects/booki.png";
-import ohMyFood from "../../Assets/Projects/ohMyFood.png";
+import ohMyFood from "../../Assets/Projects/ohmyfood.png";
 
 function Projects() {
   return (
@@ -22,12 +22,12 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={kasa}
+              imgPath={ninacarducci}
               isBlog={false}
-              title="Kasa"
-              description="Une plateforme de location immobilière développée avec React. Inclut une gestion dynamique des pages d'annonces et une interface utilisateur responsive."
-              ghLink="https://github.com/farahbed/Kasa"
-              demoLink="https://kasa-demo.vercel.app/"
+              title="Nina Carducci"
+              description="Un portfolio personnel amélioré en termes d'accessibilité, de performance et de SEO. Ce projet se concentre sur la création d'une interface utilisateur optimale et accessible tout en maximisant la performance et en optimisant les meta pour un meilleur référencement."
+              ghLink="https://github.com/farahbed/ninacarducci.git"
+              demoLink="https://ninacarducci-navy.vercel.app/"
             />
           </Col>
 
@@ -37,8 +37,8 @@ function Projects() {
               isBlog={false}
               title="Booki"
               description="Un site web de réservation d'hébergements réalisé uniquement avec HTML et CSS. Ce projet présente un design élégant et une mise en page responsive."
-              ghLink="https://github.com/farahbed/Booki"
-              demoLink="https://booki-demo.vercel.app/"
+              ghLink="https://github.com/farahbed/booki-starter-code-master.git"
+              demoLink="https://booki-starter-code-master.vercel.app/"
             />
           </Col>
 
@@ -48,8 +48,8 @@ function Projects() {
               isBlog={false}
               title="OhMyFood"
               description="Une application web dédiée à la réservation de menus dans des restaurants. Développement en HTML, CSS et animations pour améliorer l'expérience utilisateur."
-              ghLink="https://github.com/farahbed/OhMyFood"
-              demoLink="https://ohmyfood-demo.vercel.app/"
+              ghLink="https://github.com/farahbed/ohmyfood.git"
+              demoLink="https://ohmyfood-8hzofmbp6-farahbeds-projects.vercel.app/"
             />
           </Col>
         </Row>
